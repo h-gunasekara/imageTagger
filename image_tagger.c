a1d4d3f98fdd2539615fd63eceb149da73b94e93
Hamish Gunasekara <h.gunasekara@student.unimelb.edu.au>
2019-04-19 15:02:50 +1000

    adding hmtl files

--- image_tagger.c
+++ image_tagger.c
@@ -32,8 +32,13 @@ static int const HTTP_404_LENGTH = 45;
 // represents the types of method
 typedef enum
 {
-    GET,
-    POST,
+    INTRO,
+    START,
+    FIRST_TURN,
+    ACCEPTED,
+    DISCARDED,
+    ENDGAME,
+    GAMEOVER,
     UNKNOWN
 } METHOD;
 
@@ -79,11 +84,11 @@ static bool handle_http_request(int sockfd)
         ++curr;
     // assume the only valid request URI is "/" but it can be modified to accept more files
     if (*curr == ' ')
-        if (method == GET)
+        if (method == INTRO)
         {
             // get the size of the file
             struct stat st;
-            stat("lab6-GET.html", &st);
+            stat("1_intro.html", &st);
             n = sprintf(buff, HTTP_200_FORMAT, st.st_size);
             // send the header first
             if (write(sockfd, buff, n) < 0)
@@ -92,7 +97,7 @@ static bool handle_http_request(int sockfd)
                 return false;
             }
             // send the file
-            int filefd = open("lab6-GET.html", O_RDONLY);
+            int filefd = open("1_intro.html", O_RDONLY);
             do
             {
                 n = sendfile(sockfd, filefd, NULL, 2048);
@@ -106,7 +111,7 @@ static bool handle_http_request(int sockfd)
             }
             close(filefd);
         }
-        else if (method == POST)
+        else if (method == START)
         {
             // locate the username, it is safe to do so in this sample code, but usually the result is expected to be
             // copied to another buffer using strcpy or strncpy to ensure that it will not be overwritten.
@@ -117,7 +122,7 @@ static bool handle_http_request(int sockfd)
 
             // get the size of the file
             struct stat st;
-            stat("lab6-POST.html", &st);
+            stat("2-start.html", &st);
             // increase file size to accommodate the username
             long size = st.st_size + added_length;
             n = sprintf(buff, HTTP_200_FORMAT, size);
@@ -128,7 +133,7 @@ static bool handle_http_request(int sockfd)
                 return false;
             }
             // read the content of the HTML file
-            int filefd = open("lab6-POST.html", O_RDONLY);
+            int filefd = open("2-start.html", O_RDONLY);
             n = read(filefd, buff, 2048);
             if (n < 0)
             {
