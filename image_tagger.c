b75a24ed352f8b7ac978020078e9731bd1538c9a
Hamish Gunasekara <h.gunasekara@student.unimelb.edu.au>
2019-04-25 20:49:16 +1000

    printed some sock fds

--- image_tagger.c
+++ image_tagger.c
@@ -603,9 +603,12 @@ int main(int argc, char * argv[])
                     int newsockfd = accept(sockfd, (struct sockaddr *)&cliaddr, &clilen);
                     if (!player_1.sockfd){
                       player_1.sockfd = sockfd;
+                      printf("player 1:       sockfd:  %d   newsockfd:  %d      maxsockfd:     %d\n\n", sockfd, newsockfd, maxsockfd);
+
                       player_1.nwords = 0;
                     } else if (player_1.sockfd && !player_2.sockfd){
                       player_2.sockfd = sockfd;
+                      printf("player 2:       sockfd:  %d   newsockfd:  %d\n\n", sockfd, newsockfd);
                       player_2.nwords = 0;
                     }
                     if (newsockfd < 0)
