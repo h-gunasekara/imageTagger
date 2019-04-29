ddb8ff859c39b7c9e9cac686f1312554e1caa5ae
Hamish Gunasekara <h.gunasekara@student.unimelb.edu.au>
2019-04-29 11:47:48 +1000

    removed one of the changed

--- image_tagger.c
+++ image_tagger.c
@@ -421,7 +421,7 @@ static bool send_page(int sockfd, int n, char* buff, char* page, player_t* playe
   {
       char temp[strlen("Set-Cookie: username=%s\r\n") + MAXKEYLENGTH];
       sprintf(temp, "Set-Cookie: username=%s\r\n", players[curr_play_num].name);
-      size = st.st_size + players[curr_play_num].name_len  - 2;
+      size = st.st_size + players[curr_play_num].name_len  - 1;
       n = sprintf(buff, HTTP_200_FORMAT, temp, size);
   }
 
