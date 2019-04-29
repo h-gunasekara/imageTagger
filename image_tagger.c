04f8a9be03bd47c8c398b20c338a2df8d1f835d1
Hamish Gunasekara <h.gunasekara@student.unimelb.edu.au>
2019-04-29 16:48:43 +1000

    changed finish statement

--- image_tagger.c
+++ image_tagger.c
@@ -260,7 +260,7 @@ static bool handle_http_request(int sockfd, player_t* players)
               return send_page(sockfd, n, buff, ACCEPTED, players);
             }
             // if the other player should be going to end game
-            else if (players[self].finished == 1 || players[other].finished == 1)   {
+            else if (players[other].finished == 1)   {
                 players[self].finished = 0;
                 players[self].playing = 0;
                 players[other].finished = 0;
